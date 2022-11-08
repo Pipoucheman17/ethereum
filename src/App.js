da7e0bfdbb68648e8b1abb585bef1d4997a12bc1
Benjamin Levaltier <benji1705@hotmail.fr>
2022-11-08 23:13:56 +0100

    Commit init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import BuyPage from './BuyPage'
+import CollectionPage from './CollectionPage'
+import { Button } from 'react-bootstrap'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1 className="titleSection">Welcome to our NFT marketplace</h1>
+
       </header>
+
+      <div>
+        <Router>
+          <div className="App">
+          </div>
+          <Routes>
+            <Route exact path='/Buy' element={< BuyPage />}></Route>
+            <Route exact path='/contact' element={< CollectionPage />}></Route>
+          </Routes>
+
+          <div className="App">
+
+            <Link to="/Buy"><Button variant="primary" className="buttonConf">Buy NFT</Button></Link>
+            <Link to="/Collection"><Button variant="primary" className="buttonConf">Your Collection</Button></Link>
+
+
+          </div>
+        </Router>
+      </div>
     </div>
   );
 }
